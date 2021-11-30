63937f3669792cd17562c45a5865bb9592ce1dbb
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2021-11-30 13:36:32 +0100

    Moved the logic to a controllers folder

---
+++ controllers/users.js
@@ -0,0 +1,51 @@
+import {v4 as uuidv4} from "uuid";
+
+export const getUsers = (req, res) => {
+    res.send(users);
+}
+
+export const createUser = (req, res) => {
+
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send("User with the name " + user.name + " was added to the database.");
+
+};
+
+export const getUser = (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    res.send(foundUser);
+};
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id != id);
+
+    res.send("User with the id " + id + " was deleted from the database.");
+};
+
+export const patchUser = (req, res) => {
+    const { id } = req.params;
+
+    const {name, lastName, age } = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if(name){
+        user.name = name;
+    }
+    else if(lastName){
+        user.lastName = lastName;
+    }
+    else if(age){
+        user.age = age;
+    }
+
+    res.send(`User with the id ${id} was updated.`);
+};
